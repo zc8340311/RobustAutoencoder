9e25ca6a6ba343d3eb729f0dd3042f0cd1ddbdfb
Chong Zhou <czhou2@stormbringer.wpi.edu>
2018-02-12 16:22:02 -0500

    robust drop out autoencoder

--- model/SparseModels/RobustDropoutSparseAutoencoder.py
+++ model/SparseModels/RobustDropoutSparseAutoencoder.py
@@ -1,7 +1,9 @@
 import numpy as np
 import tensorflow as tf
-import SparseDropoutAutoencoder as sdae
-
+import DropoutSparseAutoencoder as sdae
+import sys
+sys.path.append("../")
+from shrink import l21shrink as SHR
 
 class RobustDropoutSparseAutoencder():
     """
@@ -11,48 +13,19 @@ class RobustDropoutSparseAutoencder():
         X = L + S
         L is a non-linearly low dimension matrix and S is a sparse matrix.
         argmin ||L - Decoder(Encoder(L))|| + ||S||_2,1
-        Where the Encoder is with dropout
+        Where the Encoder is with dropout layers to keep from overfitting
         Use Alternating projection to train model
-        The idea of shrink the l21 norm comes from the wiki 'Regularization' link: {
-            https://en.wikipedia.org/wiki/Regularization_(mathematics)
-        }
-
-
     """
     def __init__(self, sess, layers_sizes, sparsities, lambda_=1.0):
+        assert len(layers_sizes) >= 2
+        assert len(layers_sizes) - 1  == len(sparsities)
         self.lambda_ = lambda_
         self.layers_sizes = layers_sizes
-        self.sparcities = sparsities
+        self.sparsities = sparsities
         self.errors=[]
-        assert len(sparsities) == len(layers_sizes) - 1
+        self.SAE = sdae.Dropout_Sparse_Autoencoder( sess = sess, input_dim_list = self.layers_sizes,
+                                                    sparsities = self.sparsities)
 
-        self.SAE = sdae.Sparse_Dropout_Autoencoder( sess = sess, input_dim_list = self.layers_sizes,
-                                                    sparsities = self.sparcities)
-
-    def l21shrink(self, epsilon, x):
-        """
-        auther : Chong Zhou
-        Args:
-            epsilon: the shrinkage parameter
-            x: matrix to shrink on
-        Ref:
-            wiki Regularization: {https://en.wikipedia.org/wiki/Regularization_(mathematics)}
-        Returns:
-            The shrunk matrix
-        """
-        output = x.copy()
-        norm = np.linalg.norm(x, ord=2, axis=0)
-
-        for i in xrange(x.shape[1]):
-            if norm[i] > epsilon:
-                for j in xrange(x.shape[0]):
-                    output[j,i] = x[j,i] - epsilon * x[j,i] / norm[i]
-            elif norm[i] < -epsilon:
-                for j in xrange(x.shape[0]):
-                    output[j,i] = x[j,i] + epsilon * x[j,i] / norm[i]
-            else:
-                output[:,i] = 0.
-        return output
     def fit(self, X, sess, learning_rate=0.05, inner_iteration = 50,
             iteration=20, batch_size=40, verbose=False):
         ## The first layer must be the input layer, so they should have same sizes.
@@ -62,12 +35,10 @@ class RobustDropoutSparseAutoencder():
         self.S = np.zeros(X.shape)
         #LS0 = self.L + self.S
         ## To estimate the size of input X
-
         if verbose:
             print "X shape: ", X.shape
             print "L shape: ", self.L.shape
             print "S shape: ", self.S.shape
-            print "X sum value", np.linalg.norm(X,'fro')
 
         for it in xrange(iteration):
             if verbose:
@@ -83,21 +54,20 @@ class RobustDropoutSparseAutoencder():
             ## get optmized L
             self.L = self.SAE.getRecon(X = self.L, sess = sess)
             ## alternating project, now project to S and shrink S
-            self.S = self.l21shrink(self.lambda_, (X - self.L).T).T
-
-        return self.L , self.S
+            self.S = SHR.l21shrink(self.lambda_, (X - self.L).T).T
+        return self.L, self.S
 
     def transform(self, X, sess):
         return self.SAE.transform(X = X, sess = sess)
+    
     def getRecon(self, X, sess):
-        return self.SAE.getRecon(self.L, sess = sess)
+        return self.SAE.getRecon(X, sess = sess)
 
 
 if __name__ == '__main__':
-    x = np.load(r"/home/zc8304/Documents/train_x_small.pkl")
-    # x = np.array(x,dtype=float)
+    x = np.load(r"/home/czhou2/Documents/train_x_small.pkl")
     with tf.Session() as sess:
-        rsae = RobustDropoutSparseAutoencder(sess = sess, lambda_= 4000, layers_sizes=[784,784,784,784],sparsities=[0.5,0.5,0.5])
+        rsae = RobustDropoutSparseAutoencder(sess = sess, lambda_= 4000, layers_sizes=[784,784,784,784],sparsities = [0.5,0.5,0.5])
 
-        L, S = rsae.fit(x, sess = sess, inner_iteration = 40, iteration = 30,verbose = True)
+        L, S = rsae.fit(x, sess = sess, inner_iteration = 40, iteration = 10,verbose = True)
         print L.shape,S.shape

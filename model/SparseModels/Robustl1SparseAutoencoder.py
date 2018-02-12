d7a35ed53fbb021cb47b05cc50a5a45917de983f
Chong Zhou <czhou2@stormbringer.wpi.edu>
2018-02-12 18:12:43 -0500

    Robust l1 sparse autoencoder and Robust KL sparse autoencoder

--- model/SparseModels/Robustl1SparseAutoencoder.py
+++ model/SparseModels/Robustl1SparseAutoencoder.py
@@ -1,32 +1,31 @@
 import numpy as np
 import tensorflow as tf
-import KLSparseAutoencoder as sdae
+import l1SparseAutoencoder as sdae
 import sys
 sys.path.append("../")
 from shrink import l21shrink as SHR
 
-class Robust_KL_SparseAutoencder():
+class Robust_l1_SparseAutoencder():
     """
     @author: Chong Zhou
-    modified: 02/12/2018
+
     Des:
         X = L + S
         L is a non-linearly low dimension matrix and S is a sparse matrix.
-        argmin ||L - Decoder(Encoder(L))||+ KL(Encoder(L)) + ||S||_2,1
+        argmin ||L - Decoder(Encoder(L))||+ sparsity * ||Encoder(L)||_2,1 + lambda * ||S||_2,1
         Use Alternating projection to train model
- 
     """
-    def __init__(self, sess, layers_sizes, sparsity, sparse_ratio, lambda_=1.0, error = 1.0e-5):
+    def __init__(self, sess, layers_sizes, sparsity=0.4, lambda_=1.0, error = 1.0e-5):
         """
-        sparsity is the weight of penalty term 
-        sparse ratio is special for the KL divergence, how much sparsity do you expect on each hidden feature. 
+        para: 
+            sparsity: is a number that indicate the weight of the sparse penalty in the sparse autoencoder 
+            lambda_: is weight of the sparsity of S
         """
         self.lambda_ = lambda_
         self.layers_sizes = layers_sizes
         self.sparcity = sparsity
-        self.sparse_ratio = sparse_ratio
-        self.SAE = sdae.KL_Sparse_Autoencoder( sess = sess, input_dim_list = self.layers_sizes,
-                                                    sparsity = self.sparcity, sparse_ratio = self.sparse_ratio)
+        self.SAE = sdae.l1_Sparse_Autoencoder(sess = sess, input_dim_list = self.layers_sizes,
+                                                    sparsity = self.sparcity)
 
     def fit(self, X, sess, learning_rate=0.05, inner_iteration = 50,
             iteration=20, batch_size=40, verbose=False):
@@ -35,12 +34,12 @@ class Robust_KL_SparseAutoencder():
         ## initialize L, S, mu(shrinkage operator)
         self.L = np.zeros(X.shape)
         self.S = np.zeros(X.shape)
-        
+
         if verbose:
             print "X shape: ", X.shape
             print "L shape: ", self.L.shape
             print "S shape: ", self.S.shape
-            
+
         for it in xrange(iteration):
             if verbose:
                 print "Out iteration: " , it
@@ -54,8 +53,9 @@ class Robust_KL_SparseAutoencder():
                                     verbose = verbose)
             ## get optmized L
             self.L = self.SAE.getRecon(X = self.L, sess = sess)
-            ## alternating project, now project to S and shrink S.T
+            ## alternating project, now project to S and shrink S
             self.S = SHR.l21shrink(self.lambda_, (X - self.L).T).T
+
         return self.L, self.S
 
     def transform(self, X, sess):
@@ -63,11 +63,11 @@ class Robust_KL_SparseAutoencder():
     def getRecon(self, X, sess):
         return self.SAE.getRecon(self.L, sess = sess)
 
+
 if __name__ == '__main__':
     x = np.load(r"/home/czhou2/Documents/train_x_small.pkl")
     with tf.Session() as sess:
-        rsae = Robust_KL_SparseAutoencder(sess = sess, lambda_= 40, layers_sizes=[784,784,784,784],
-                                            sparsity= 0.5, sparse_ratio= 0.2)
+        rsae = Robust_l1_SparseAutoencder(sess = sess, lambda_= 4, layers_sizes=[784,784,784,784],sparsity=0.5)
 
         L, S = rsae.fit(x, sess = sess, inner_iteration = 20, iteration = 30,verbose = True)
         print L.shape,S.shape
